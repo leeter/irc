cb7a8840b38da0d1acad2199641e1f77a948ca3f
leeter <leeterfp@gmail.com>
2013-05-18 08:56:04 -0600

    Add basic behavior tests for nickname
    These will need to be refactored in the future but for now they provide
    basic validation that the behavior is what we should expect. That the
    value object enforces its own business logic

---
+++ irctest/NickNameTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace irctest
+{
+    using irc.protocol;
+
+    [TestClass]
+    public class NickNameTests
+    {
+        [TestMethod]
+        public void TestConstruction()
+        {
+            var testString = "leeter";
+            var nickName = new NickName(testString);
+            Assert.AreEqual(testString, nickName.Value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBadNick()
+        {
+            var testString = "-leeter";
+            var nickName = new NickName(testString);
+            Assert.AreEqual(testString, nickName.Value);
+        }
+    }
+}
